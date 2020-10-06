a491a943a957cad8507e537053da4264e2eab3e7
IvanVarvarchuk <vivan2101@gmail.com>
2020-10-06 16:37:10 +0300

    Initial algorithm

---
+++ lab1/VarvarchukIvan.RobotChallange/DistanceHelper.cs
@@ -0,0 +1,52 @@
+﻿using Robot.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VarvarchukIvan.RobotChallange
+{
+    public class DistanceHelper
+    {
+        public static int FindDistance(Position a, Position b) => (int)(Math.Pow(a.X - b.X, 2) + Math.Pow(a.Y - b.Y, 2));
+        
+
+        public Position FindNearestFreeStation(Robot.Common.Robot movingRobot, Map map, IList<Robot.Common.Robot> robots)
+        {
+            EnergyStation nearest = null;
+            int minDistance = int.MaxValue;
+            foreach (var station in map.Stations)
+            {
+                if (IsStationFree(station, movingRobot, robots))
+                {
+                    int d = DistanceHelper.FindDistance(station.Position, movingRobot.Position);
+                    if (d < minDistance)
+                    {
+                        minDistance = d;
+                        nearest = station;
+                    }
+                }
+            }
+            return nearest == null ? null : nearest.Position;
+        }
+        public bool IsStationFree(EnergyStation station, Robot.Common.Robot movingRobot,
+       IList<Robot.Common.Robot> robots)
+        {
+            return IsCellFree(station.Position, movingRobot, robots);
+        }
+        public bool IsCellFree(Position cell, Robot.Common.Robot movingRobot, IList<Robot.Common.Robot> robots)
+        {
+            foreach (var robot in robots)
+            {
+                if (robot != movingRobot)
+                {
+                    if (robot.Position == cell)
+                        return false;
+                }
+            }
+            return true;
+        }
+
+    }
+}
---
+++ lab1/VarvarchukIvan.RobotChallange/VarvarchukIvanAlgorithm.cs
@@ -0,0 +1,36 @@
+﻿using Robot.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VarvarchukIvan.RobotChallange
+{
+    class VarvarchukIvanAlgorithm : IRobotAlgorithm
+    {
+        public string Author => "Ivan Varvarchuk";
+
+        public RobotCommand DoStep(IList<Robot.Common.Robot> robots, int robotToMoveIndex, Map map)
+        {
+            Robot.Common.Robot movingRobot = robots[robotToMoveIndex];
+            if ((movingRobot.Energy > 500) && (robots.Count < map.Stations.Count))
+            {
+                return new CreateNewRobotCommand();
+            }
+
+            Position stationPosition = new DistanceHelper().FindNearestFreeStation(robots[robotToMoveIndex], map, robots);
+            
+             if (stationPosition == null)
+                return null;
+            if (stationPosition == movingRobot.Position)
+                return new CollectEnergyCommand();
+            else
+            {
+
+                return new MoveCommand() { NewPosition = stationPosition };
+            }
+        }
+
+    }
+}
